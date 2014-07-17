15887253fce1798af0a368e25094dcdd1fb0bf79
Grant Carpenter <grant@g12r.com>
2014-07-17 09:58:36 -0700

    Added tmux.conf to relink

--- relink.sh
+++ relink.sh
@@ -2,3 +2,4 @@ ln -f ~/.dotfiles/gc.zsh-theme ~/.oh-my-zsh/themes/gc.zsh-theme
 ln -f ~/.dotfiles/gc_rhel.zsh-theme ~/.oh-my-zsh/themes/gc_rhel.zsh-theme
 ln -f ~/.dotfiles/.zshrc ~/.zshrc
 ln -f ~/.dotfiles/.config ~/.ssh/.config
+ln -f ~/.dotfiles/.tmux.conf ~/.tmux.conf
