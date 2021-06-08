81e1dd2f6e3683d7e3a1a1aa4325f3e86bf22d69
Grant C <grant@g12r.com>
2021-06-07 17:51:48 -0700

    Adding kitty config files

--- relink.sh
+++ relink.sh
@@ -1,6 +1,12 @@
+# zsh and oh-my-zsh
 ln -f ~/.dotfiles/gc.zsh-theme ~/.oh-my-zsh/themes/gc.zsh-theme
 ln -f ~/.dotfiles/gc_rhel.zsh-theme ~/.oh-my-zsh/themes/gc_rhel.zsh-theme
 ln -f ~/.dotfiles/.zshrc ~/.zshrc
 ln -f ~/.dotfiles/.zshenv ~/.zshenv
+
+# kitty
+ln -sf ~/.dotfiles/kitty ~/.config/kitty
+
+# Inactive
 #ln -f ~/.dotfiles/.config ~/.ssh/.config
 #ln -f ~/.dotfiles/.tmux.conf ~/.tmux.conf
